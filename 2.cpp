46b66bc7342f07c84532336b9398d361fb428c00
Chang smallfox <pikacsie1213@gmail.com>
2016-09-05 20:41:58 +0800

    2. Unique Paths

---
+++ 2.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        int solve[m][n];
+        for(int i = 0; i < m; ++i){
+            for(int j = 0; j < n; ++j){
+                if( i == 0  || j == 0)
+                    solve[i][j] = 1;
+                else
+                solve[i][j] = solve[i-1][j] + solve[i][j-1];
+            }
+        }
+        return solve[m-1][n-1];
+    }
+};
