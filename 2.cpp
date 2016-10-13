8548e3db2da4f8210471537567b98abec2e84c30
Chan Smallfox <pikacsie1213@gmail.com>
2016-10-13 17:38:14 +0800

    Unique Paths

--- 2.cpp
+++ 2.cpp
@@ -4,7 +4,7 @@ public:
         int solve[m][n];
         for(int i = 0; i < m; ++i){
             for(int j = 0; j < n; ++j){
-                if( i == 0  || j == 0)
+                if( i == 0 || j == 0)
                     solve[i][j] = 1;
                 else
                 solve[i][j] = solve[i-1][j] + solve[i][j-1];
